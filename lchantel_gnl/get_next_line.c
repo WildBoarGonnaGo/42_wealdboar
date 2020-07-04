06cc285d58ef06e8e5cdb8f42771a536705b535b
lchantel <lchantel@student.21-school.ru>
2020-07-04 13:01:21 +0300

    gnl_with_bonus_040720

--- lchantel_gnl/get_next_line.c
+++ lchantel_gnl/get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lchantel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 19:52:36 by lchantel          #+#    #+#             */
-/*   Updated: 2020/07/04 12:39:15 by lchantel         ###   ########.fr       */
+/*   Updated: 2020/07/04 11:45:03 by lchantel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int		ft_check_fd(int fd, int read_byte, char *buff_str_rest, char **line)
+int		ft_check_fd(int fd, int read_byte, char **buff_str_rest, char **line)
 {
 	if (fd < 0 || read_byte < 0)
 	{
@@ -22,7 +22,7 @@ int		ft_check_fd(int fd, int read_byte, char *buff_str_rest, char **line)
 			free(*line);
 		return (-1);
 	}
-	else if (!read_byte && !buff_str_rest)
+	else if (!read_byte && !*(buff_str_rest + fd))
 		return (0);
 	else
 		return (1);
@@ -30,13 +30,13 @@ int		ft_check_fd(int fd, int read_byte, char *buff_str_rest, char **line)
 
 int		get_next_line(int fd, char **line)
 {
-	static char	*buff_str_rest;
+	static char	*buff_str_rest[1024];
 	char		buff_str[BUFFER_SIZE + 1];
 	int			read_byte;
 	int			nl_stat;
 
 	read_byte = 0;
-	if (!(ft_gnl_read_rest(line, &buff_str_rest)))
+	if (!(ft_gnl_read_rest(line, buff_str_rest + fd)))
 	{
 		while ((read_byte = read(fd, buff_str, BUFFER_SIZE)) > 0)
 		{
@@ -46,8 +46,8 @@ int		get_next_line(int fd, char **line)
 				buff_str[nl_stat] = 0;
 			*line = ft_gnl_strjoin(*line, buff_str);
 			if (nl_stat || !buff_str[nl_stat])
-				buff_str_rest = ft_gnl_strjoin(buff_str_rest, buff_str +
-				ft_gnl_seekchar(buff_str, 0) + 1);
+				*(buff_str_rest + fd) = ft_gnl_strjoin(*(buff_str_rest + fd),
+				buff_str + ft_gnl_seekchar(buff_str, 0) + 1);
 			if (nl_stat || !buff_str[nl_stat])
 				break ;
 		}
