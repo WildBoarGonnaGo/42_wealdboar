83f25a192b769592845a956b764d055a5c9c8307
WildBoarGonnaGo <unknownthrill814@gmail.com>
2021-01-04 14:29:43 +0300

    minishell: very start

--- minishell/train/pipe.c
+++ minishell/train/pipe.c
@@ -1,2 +1,11 @@
+#include <unistd.h>
 #include <stdio.h>
-
+int main(int argc, char *argv[])
+{
+	/*pipe (канал) - файл, который является буфером с которого можно
+	 считывать и в которой можно записывать*/
+	int fd[2];
+	if (pipe(fd) == -1)
+		printf("An error occured with these file descriptors");
+	return (0);
+}
