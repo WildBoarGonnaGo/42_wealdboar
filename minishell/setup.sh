60092ce795c3efcc2acad58be4bc695cbc750d39
WildBoarGonnaGo <60565993+WildBoarGonnaGo@users.noreply.github.com>
2021-03-07 20:55:34 +0300

    Update setup.sh

--- minishell/setup.sh
+++ minishell/setup.sh
@@ -3,4 +3,4 @@
 clang -Wall -Wextra -Werror -g main.c sh_envp_search.c sh_line_ansys.c alloc_free_2.c \
 execve_args.c sh_gnl.c sh_user_bin.c addchar.c memrealloc.c sh_parcer.c sh_spec_cases.c \
 sh_redirects.c sh_env_linefix.c sh_free_str.c sh_parcer_envp_fix.c sh_pipe_block_fix.c \
-sh_ignore_colon.c ./func_dir/*.c -o minishell -L./libc -lft 
+sh_ignore_colon.c sh_line_err_parse.c sh_err_analisys.c ./func_dir/*.c -o minishell -L./libc -lft 
